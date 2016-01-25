f72d4055bf217941d8e329173c6be387cb5b5477
Kinjo <amsp8b@mst.edu>
2016-01-25 00:30:07 -0600

    Now uses pygame to visualize the grid

--- color-connect.py
+++ color-connect.py
@@ -3,23 +3,46 @@
 
 import agent
 import map
+import sys 
 from sys import argv
+
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((640,480))
+
+color = (255,0,0)
+
+colorRed = (255,0,0)
+colorGreen = (0,255,0)
+colorBlue = (0,0,255)
+colorDarkBlue = (0,0,128)
+colorWhite = (255,255,255)
+colorBlack = (0,0,0)
+colorPink = (255,200,200)
+
+colorList = [colorRed, colorBlue]
+
+
+
+pygame.display.update()
 
 # Set config filepath
 cfgPath = ''
 if len(argv) < 2:
-  print "Please specify a file path for the puzzle instance text file."
+  print("Please specify a file path for the puzzle instance text file.")
   exit()
 else:
 	cfgPath = argv[1]
   
-print "Creating grid..."
+print("Creating grid...")
 gridInput = []
 with open(cfgPath) as input:
   for line in input:
     gridInput.append(line.split())
 	
-print gridInput
+print(gridInput)
 
 gridSize = int(gridInput[0][0])
 numberOfColors = int(gridInput[0][1])
@@ -30,3 +53,34 @@ percept = None
 
 # action = myAgent.Action(percept)
 
+thickness = 0
+x = 100
+y = 100
+boxWidth = 50
+
+while True:
+
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+      
+
+  screen.fill(colorBlack)
+  
+  for i in range(gridSize):
+    for j in range(gridSize):
+      currentColor = grid[i][j].colored
+      if currentColor == "e":
+        currentColor = colorBlack
+      else:
+        currentColor = colorList[int(currentColor)]
+        
+      if grid[i][j].startPoint is True or grid[i][j].endPoint is True:
+        pygame.draw.circle(screen, currentColor, (x+(boxWidth*i),y+(boxWidth*j)), 25, thickness)
+      else:
+        pygame.draw.rect(screen, currentColor, (x+(boxWidth*i),y+(boxWidth*j),boxWidth,boxWidth), thickness)
+   
+
+  # update the screen
+  pygame.display.update()
--- map.py
+++ map.py
@@ -6,8 +6,8 @@ class Tile():
     self.x = x
     self.y = y
     self.colored = c
-    self.startPoint = False
-    self.endPoint = False
+    self.startPoint = True
+    self.endPoint = True
   
   def __str__(self):
     return str(self.x) + "," + str(self.y) + ": " + str(self.colored)
@@ -16,11 +16,11 @@ class Tile():
 def CreateMap(width, height, input):
   index = 3
   world = []
-  for i in xrange(width):
+  for i in range(width):
     world.append([])
-    for j in xrange(height):
+    for j in range(height):
       world[i].append(Tile(i, j, input[i+1][j]))
-      print str(i) + "," + str(j) + ": " + str(input[i+1][j])
+      print (str(i) + "," + str(j) + ": " + str(input[i+1][j]))
       index = index + 1
     
   return world
